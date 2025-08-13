5c924d888b49826de581def1722496aad3061614
Vaishnavi212 <vaishnavisinnur@gmail.com>
2025-08-13 17:13:09 +0530

    Switch to HashRouter for GitHub Pages to fix 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/InvisiLock_main">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
