7a4b3df67746dc168ad0585f2ba6a43000fa53d8
Vaishnavi212 <vaishnavisinnur@gmail.com>
2025-08-13 17:08:18 +0530

    Update React Router basename and Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/InvisiLock_main/",
+    base: "/InvisiLock_main/", 
 
   server: {
     host: "::",
